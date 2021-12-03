a603debf2c90f3fd2ed612f3a2bac9efe9e24ce6
joorani <shinerani16@gmail.com>
2021-12-03 11:03:46 +0900

    Refactoring Restaurant

--- src/main/java/com/example/fooddeliveryapi/service/RestaurantService.java
+++ src/main/java/com/example/fooddeliveryapi/service/RestaurantService.java
@@ -4,6 +4,7 @@ import com.example.fooddeliveryapi.dto.RestaurantRequestDto;
 import com.example.fooddeliveryapi.dto.RestaurantResponseDto;
 import com.example.fooddeliveryapi.model.Restaurant;
 import com.example.fooddeliveryapi.repository.RestaurantRepository;
+import com.example.fooddeliveryapi.validater.RestaurantValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,22 +26,8 @@ public class RestaurantService {
     @Transactional
     public RestaurantResponseDto registerRestaurant(RestaurantRequestDto restaurantRequestDto) {
 
-        if(restaurantRequestDto.getMinOrderPrice() < 1000 || restaurantRequestDto.getMinOrderPrice() > 100000) {
-            throw new IllegalArgumentException("최소 1000원, 최대 100000원 사이로 입력하세요.");
-        }
-
-        if(restaurantRequestDto.getMinOrderPrice() % 100 != 0) {
-            throw new IllegalArgumentException("최소주문 금액은 100원 단위로만 입력 가능합니다.");
-        }
-
-        //기본 배달비(deliveryFee) 허용값: 0~10,0000원
-        if (restaurantRequestDto.getDeliveryFee() < 0 || restaurantRequestDto.getDeliveryFee() > 10000) {
-           throw new IllegalArgumentException("0~10000원 사이로 입력하세요.");
-        }
-
-        if(restaurantRequestDto.getDeliveryFee() % 500 != 0) {
-            throw new IllegalArgumentException("기본 배달비는 500원 단위로만 입력 가능합니다.");
-        }
+        //입력값 Validation
+        RestaurantValidator.validateRestaurantRegister(restaurantRequestDto);
 
         Restaurant restaurant = restaurantRepository.save(
                 Restaurant.builder()
--- src/main/java/com/example/fooddeliveryapi/validater/RestaurantValidator.java
+++ src/main/java/com/example/fooddeliveryapi/validater/RestaurantValidator.java
@@ -1,50 +1,29 @@
 package com.example.fooddeliveryapi.validater;
 
-import com.example.fooddeliveryapi.dto.RestaurantDto;
+import com.example.fooddeliveryapi.dto.RestaurantRequestDto;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
-import org.springframework.validation.Validator;
 
 @Component
-public class RestaurantValidator implements Validator {
+public class RestaurantValidator{
 
-    @Override
-    public boolean supports(Class clazz) {
-        return RestaurantDto.class.equals(clazz);
-    }
 
-    @Override
-    public void validate(Object obj, Errors e) {
-        RestaurantDto restaurantDto = (RestaurantDto) obj;
+    public static void validateRestaurantRegister(RestaurantRequestDto restaurantRequestDto) {
+        if(restaurantRequestDto.getMinOrderPrice() < 1000 || restaurantRequestDto.getMinOrderPrice() > 100000) {
+            throw new IllegalArgumentException("최소 1000원, 최대 100000원 사이로 입력하세요.");
+        }
 
-        //음식점 이름 빈칸 확인
-        ValidationUtils.rejectIfEmpty(e, "name",
-                "Restaurant.name empty",
-                "음식점 이름을 입력해주세요.");
-
-        //최소주문 확인 허용값 1,000~100,000
-        ValidationUtils.rejectIfEmpty(e,"minOrderPrice", "Restaurant.minOrderPrice empty",
-                "최소주문 가격을 입력해주세요.");
-
-        if(restaurantDto.getMinOrderPrice() < 1000) {
-            e.rejectValue("minOrderPrice", "minOrderPrice invalid","최소주문 금액은 1,000원 이상이어야 합니다." );
-        } else if (restaurantDto.getMinOrderPrice() > 100000) {
-            e.rejectValue("minOrderPrice", "minOrderPrice invalid","최소주문 금액은 1,000,000을 초과할 수 없습니다." );
-        } else if(restaurantDto.getMinOrderPrice() % 100 != 0) {
-            e.rejectValue("minOrderPrice", "minOrderPrice invalid","최소주문 금액은 100원 단위로만 입력 가능합니다." );
+        if(restaurantRequestDto.getMinOrderPrice() % 100 != 0) {
+            throw new IllegalArgumentException("최소주문 금액은 100원 단위로만 입력 가능합니다.");
         }
 
         //기본 배달비(deliveryFee) 허용값: 0~10,0000원
-        ValidationUtils.rejectIfEmpty(e, "deliveryFee", "Restaurant.deliveryFee empty", "기본 배달비를 입력해주세요.");
-
-        if (restaurantDto.getDeliveryFee() < 0) {
-            e.rejectValue("deliveryFee", "deliveryFee invalid", "기본 배달비는 0원 이상부터 입력 가능합니다.");
-        } else if(restaurantDto.getDeliveryFee() > 10000) {
-            e.rejectValue("deliveryFee", "deliveryFee invalid", "기본 배달비는 10000원을 초과할 수 없습니다.");
-        } else if(restaurantDto.getDeliveryFee() % 500 != 0) {
-            e.rejectValue("deliveryFee", "deliveryFee invalid", "기본 배달비는 500원 단위로만 입력 가능합니다.");
+        if (restaurantRequestDto.getDeliveryFee() < 0 || restaurantRequestDto.getDeliveryFee() > 10000) {
+            throw new IllegalArgumentException("0~10000원 사이로 입력하세요.");
         }
 
+        if(restaurantRequestDto.getDeliveryFee() % 500 != 0) {
+            throw new IllegalArgumentException("기본 배달비는 500원 단위로만 입력 가능합니다.");
+        }
     }
 }
+
